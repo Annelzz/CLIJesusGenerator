dfbba998b46afbefcbfbffcaabf7e663bb85def9
Annelise <annelzz26@gmail.com>
2020-12-02 20:19:47 -0800

    added API things!

--- lib/JesusGenerator/api.rb
+++ lib/JesusGenerator/api.rb
@@ -4,16 +4,20 @@ require 'net/http'
 require 'json'
 
 
-
 class API
 
-
-    def self.get_gospel(gospel, chapter, verse)
-        url = "http://getbible.net/json?scrip=#{gospel} #{chapter}:#{verse}"
-        uri = URI.parse(url)
+    def self.get_gospel(book, section, phrase)
+        url = "https://getbible.net/json?scrip=#{book}#{section}:#{phrase}/"
+        uri = URI(url)
         response = Net::HTTP.get_response(uri)
-        response.body
-        JSON.parse(response.body)
+        bodied = response.body()
+        takenout = bodied.slice!(1..(bodied.length-3))
+        parsed = JSON.parse(takenout)
+        puts parsed
+        final = parsed.verse
+
+        
+       
       
     end
         
--- lib/JesusGenerator/cli.rb
+++ lib/JesusGenerator/cli.rb
@@ -1,14 +1,19 @@
 class CLI
 
+    attr_accessor :gospel, :chapter, :verse_number
 
     def start
-        puts "Welcome to my app!"
-        puts "Please enter a Gospel(Matthew, Mark, Luke, John)"
-        gospel = gets.chomp
-        puts "Now enter a Chapter Number(ex. 15)"
-        chapter = gets.to-i
-        puts "Now enter a Verse Number(ex. 3)"
-        verse = gets.to-i
-        API.get_gospel(gospel, chapter, verse)
+        # puts "Welcome to my app!"
+        # puts "Please enter a Gospel(Matthew, Mark, Luke, John)"
+        # gospel = gets.chomp
+        # puts "Now enter a Chapter Number(ex. 15)"
+        # chapter = gets.to_i
+        # puts "Now enter a Verse Number(ex. 3)"
+        # verse_number = gets.to_i
+        gospel = "Luke"
+        chapter = 1
+        verse_number = 1
+        API.get_gospel(gospel, chapter, verse_number)
+        puts "#{final}"
     end
 end
